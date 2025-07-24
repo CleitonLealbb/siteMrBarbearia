3c02d9dcc074ff3bc94d4834f2c42eef7e709d28
cleitonleal_ofc <cleiton_nx@hotmail.com>
2025-07-24 15:23:47 -0300

    fist commit

---
+++ src/assets/javascript/javascript.js
@@ -0,0 +1,13 @@
+// função para mudar imagem de fundo
+let currentIndex = 0;
+function changeBackgroundImage() {
+    const images = [
+        'url("./src/assets/images/imagem1.jpg")',
+        'url("./src/assets/images/imagem2.jpg")',
+        
+    ];
+    
+    document.querySelector('.section--story-imgs').style.backgroundImage = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+
+}setInterval(changeBackgroundImage, 3000);
